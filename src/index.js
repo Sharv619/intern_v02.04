b7b04d04af4a37b97e42da61e346c5e75daf1c21
Sharv619 <hlade03@gmail.com>
2023-11-28 22:30:40 +1100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,34 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Navbar from './components/Navbar';
+import Caraosal from './components/Caraosal';
+import Campaign from './components/Campaign';
+import Members from './components/Members';
+import Login from './components/Login';
+import Donation from './components/Donation';
+import CreateCampaign from './components/CreateCampaign';
+import AboutUs from './components/AboutUs';
+import ContactUs from './components/ContactUs';
+import Footer from './components/Footer';
 
 root.render(
   <React.StrictMode>
     <App />
+    <Navbar />
+    <Caraosal />
+    <Campaign />
+    <Members />
+    <Login />
+    <Donation />
+    <CreateCampaign />
+    <AboutUs />
+    <ContactUs />
+
+    <Footer />
+
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
